241d75b7b0b503c2a5ded2503f7f60bb73cca0c7
daniel calliari <dbcalliari@gmail.com>
2025-12-22 21:41:53 -0300

    feat: add events and tickets tables to database schema

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,6 +1,8 @@
 import { sql } from "drizzle-orm";
 import {
+	integer,
 	pgSchema,
+	text,
 	timestamp,
 	unique,
 	uuid,
@@ -20,3 +22,28 @@ export const usersInTickzi = tickzi.table(
 	},
 	(table) => [unique("users_email_key").on(table.email)],
 );
+
+export const eventsInTickzi = tickzi.table("events", {
+	id: uuid().primaryKey().defaultRandom(),
+	user_id: uuid()
+		.notNull()
+		.references(() => usersInTickzi.id, { onDelete: "cascade" }),
+	title: varchar({ length: 255 }).notNull(),
+	description: text(),
+	date: timestamp({ mode: "string" }).notNull(),
+	location: varchar({ length: 255 }).notNull(),
+	ticket_quantity: integer().notNull(),
+	ticket_price: integer().notNull(),
+	created_at: timestamp({ mode: "string" }).default(sql`CURRENT_TIMESTAMP`),
+});
+
+export const ticketsInTickzi = tickzi.table("tickets", {
+	id: uuid().primaryKey().defaultRandom(),
+	event_id: uuid()
+		.notNull()
+		.references(() => eventsInTickzi.id, { onDelete: "cascade" }),
+	user_id: uuid()
+		.notNull()
+		.references(() => usersInTickzi.id, { onDelete: "cascade" }),
+	purchased_at: timestamp({ mode: "string" }).default(sql`CURRENT_TIMESTAMP`),
+});
